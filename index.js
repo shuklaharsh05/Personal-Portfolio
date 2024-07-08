06a447c54e54adfe82607ecad9bbca72ad024402
shuklaharsh05 <126856278+shuklaharsh05@users.noreply.github.com>
2024-07-08 21:27:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ function closemenu(){
 }
 
 // Form Submit
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxob2HH2ua8bA_lwGasNJ392OmRoXr58VofiXjlmsRo29_MdS0TmQGCxA8ypYfi9CgLeQ/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbxob2HH2ua8bA_lwGasNJ392OmRoXr58VofiXjlmsRo29_MdS0TmQGCxA8ypYfi9CgLeQ/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg")
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//         msg.innerHTML = "Message sent successfully"
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         },5000)
+//         form.reset()
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
 // Changing Text(Home part)
     const typed = new Typed('.multiple-text', {
 strings: ['Frontend Developer', 'UI/UX Designer', 'Web Developer'],
